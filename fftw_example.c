10501b6792ef31eb96836e1809c9ac0d4594e387
J. S. Oishi <jsoishi@gmail.com>
2017-10-12 22:19:07 -0400

    use malloc(), so we can make very big problems for performance testing.

--- fftw_example.c
+++ fftw_example.c
@@ -1,8 +1,8 @@
 /* Start reading here */
 
 #include <fftw3.h>
-
-#define NUM_POINTS 64
+#include <stdlib.h>
+#define NUM_POINTS 10000000
 
 
 /* Never mind this bit */
@@ -13,15 +13,14 @@
 #define REAL 0
 #define IMAG 1
 
-void acquire_from_somewhere(fftw_complex* signal) {
+void acquire_from_somewhere(fftw_complex* signal, int size) {
     /* Generate two sine waves of different frequencies and
      * amplitudes.
      */
 
     int i;
-    for (i = 0; i < NUM_POINTS; ++i) {
-        double theta = (double)i / (double)NUM_POINTS * M_PI;
-
+    for (i = 0; i < size; ++i) {
+        double theta = (double)i / (double)size * M_PI;
         signal[i][REAL] = 1.0 * cos(10.0 * theta) +
                           0.5 * cos(25.0 * theta);
 
@@ -30,13 +29,13 @@ void acquire_from_somewhere(fftw_complex* signal) {
     }
 }
 
-void do_something_with(fftw_complex* result) {
+void do_something_with(fftw_complex* result, int size) {
     int i;
-    for (i = 0; i < NUM_POINTS; ++i) {
+    for (i = 0; i < size; ++i) {
         double mag = sqrt(result[i][REAL] * result[i][REAL] +
                           result[i][IMAG] * result[i][IMAG]);
 
-        printf("%g\n", mag);
+        //printf("%g\n", mag);
     }
 }
 
@@ -44,20 +43,23 @@ void do_something_with(fftw_complex* result) {
 /* Resume reading here */
 
 int main() {
-    fftw_complex signal[NUM_POINTS];
-    fftw_complex result[NUM_POINTS];
+  int size = NUM_POINTS;
+  fftw_complex *signal = malloc(size * sizeof(fftw_complex));
+  fftw_complex *result = malloc(size * sizeof(fftw_complex));
 
-    fftw_plan plan = fftw_plan_dft_1d(NUM_POINTS,
-                                      signal,
-                                      result,
-                                      FFTW_FORWARD,
-                                      FFTW_ESTIMATE);
-
-    acquire_from_somewhere(signal);
-    fftw_execute(plan);
-    do_something_with(result);
-
-    fftw_destroy_plan(plan);
+  
+    
+  fftw_plan plan = fftw_plan_dft_1d(size,
+                                    signal,
+                                    result,
+                                    FFTW_FORWARD,
+                                    FFTW_ESTIMATE);
+  
+  acquire_from_somewhere(signal, size);
+  fftw_execute(plan);
+  do_something_with(result, size);
+  
+  fftw_destroy_plan(plan);
 
     return 0;
 }
